bff0ab8cf1ac8df884da51a5bd419ee2860bcd6d
Andrew <drewg3r@gmail.com>
2022-01-16 00:00:29 +0200

    Update index page test

--- app/tests/test_index.py
+++ app/tests/test_index.py
@@ -1,7 +1,4 @@
 def test_index_page(client):
     """Test index page accessibility"""
     response = client.get('/')
-    data = response.data.decode('UTF-8')
-
     assert response.status_code == 200
-    assert 'index' in data
